2bc9d3b4417d770afca74d6f5dd04bd0f3ea27d6
Harry Du <xdu@microstrategy.com>
2021-03-11 18:34:24 -0500

    1.Changing to annoymous auth. 2. Adding website into site so it can be used by github pages

---
+++ site/precache-manifest.3f81545901ee975423ca0e1b25d37626.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "3bdc2e6c1eef647f02e5",
+    "url": "./static/css/main.9032e7d0.chunk.css"
+  },
+  {
+    "revision": "3bdc2e6c1eef647f02e5",
+    "url": "./static/js/main.3bdc2e6c.chunk.js"
+  },
+  {
+    "revision": "451d02ef0af349129052",
+    "url": "./static/css/1.f370e53e.chunk.css"
+  },
+  {
+    "revision": "451d02ef0af349129052",
+    "url": "./static/js/1.451d02ef.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "./static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "./static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "./static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "./static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "./static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "e8401725d3f52d9c1535ce6c711c44d9",
+    "url": "./index.html"
+  }
+];
