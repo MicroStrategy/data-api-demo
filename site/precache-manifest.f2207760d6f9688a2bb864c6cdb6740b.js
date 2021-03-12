cab102def037d5ba2a4b406361fbcfaf1a50448d
Harry Du <xdu@microstrategy.com>
2021-03-11 20:05:21 -0500

    updating generated the site

--- site/precache-manifest.f2207760d6f9688a2bb864c6cdb6740b.js
+++ site/precache-manifest.f2207760d6f9688a2bb864c6cdb6740b.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "502356e363065fd40060",
-    "url": "./static/css/main.4cedecf7.chunk.css"
+    "revision": "0644f1b06f4356df6fca",
+    "url": "./static/css/main.ff5c78cf.chunk.css"
   },
   {
-    "revision": "502356e363065fd40060",
-    "url": "./static/js/main.502356e3.chunk.js"
+    "revision": "0644f1b06f4356df6fca",
+    "url": "./static/js/main.0644f1b0.chunk.js"
   },
   {
     "revision": "451d02ef0af349129052",
@@ -20,6 +20,10 @@ self.__precacheManifest = [
     "url": "./static/js/runtime~main.4a686d48.js"
   },
   {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "./static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
     "revision": "fa2772327f55d8198301fdb8bcfc8158",
     "url": "./static/media/glyphicons-halflings-regular.fa277232.woff"
   },
@@ -28,19 +32,15 @@ self.__precacheManifest = [
     "url": "./static/media/glyphicons-halflings-regular.e18bbf61.ttf"
   },
   {
-    "revision": "f4769f9bdb7466be65088239c12046d1",
-    "url": "./static/media/glyphicons-halflings-regular.f4769f9b.eot"
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "./static/media/glyphicons-halflings-regular.89889688.svg"
   },
   {
     "revision": "448c34a56d699c29117adc64c43affeb",
     "url": "./static/media/glyphicons-halflings-regular.448c34a5.woff2"
   },
   {
-    "revision": "89889688147bd7575d6327160d64e760",
-    "url": "./static/media/glyphicons-halflings-regular.89889688.svg"
-  },
-  {
-    "revision": "561abe908c43006587433cb80479b0e7",
+    "revision": "1147e0e01015afa6baa27e69bfce33a8",
     "url": "./index.html"
   }
 ];
--- site/service-worker.js
+++ site/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.c9090f652bfe2419feba79159f23adc4.js"
+  "./precache-manifest.f2207760d6f9688a2bb864c6cdb6740b.js"
 );
 
 workbox.clientsClaim();
